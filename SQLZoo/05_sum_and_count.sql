d529a7676225f4ca2a2e47fd9d7e822c19f31576
Sara Pesavento <sarapesavento@gmail.com>
2019-03-07 11:52:41 -0800

    SUM and COUNT

---
+++ SQLZoo/05_sum_and_count.sql
@@ -0,0 +1,32 @@
+-- https://sqlzoo.net/wiki/SUM_and_COUNT
+
+-- 1. Show the total population of the world.
+SELECT sum(population) AS total_pop FROM world;
+
+-- 2. List all the continents - just once each.
+SELECT DISTINCT(continent) FROM world;
+
+-- 3. Give the total GDP of Africa.
+SELECT sum(gdp) AS Africa_gdp FROM world
+WHERE continent = 'Africa';
+
+-- 4. How many countries have an area of at least 1000000.
+SELECT COUNT(name) AS count_big FROM world
+WHERE area >= 1000000;
+
+-- 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania').
+SELECT sum(population) AS total_pop FROM world
+WHERE name IN('Estonia', 'Latvia', 'Lithuania');
+
+-- 6. For each continent show the continent and number of countries.
+SELECT continent, COUNT(name) AS country_count FROM world
+GROUP BY continent;
+
+-- 7. For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT continent, COUNT(DISTINCT name) FROM world
+WHERE population > 10000000
+GROUP BY continent;
+
+-- 8. List the continents that have a total population of at least 100 million.
+SELECT continent FROM world
+GROUP BY continent HAVING SUM(population) >= 100000000;
