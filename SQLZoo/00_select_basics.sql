3c757cd2310e34b54e23d4489862570251394e60
Sara Pesavento <sarapesavento@gmail.com>
2019-03-07 11:37:16 -0800

    SELECT basics

--- SQLZoo/00_select_basics.sql
+++ SQLZoo/00_select_basics.sql
@@ -1,8 +1,10 @@
+-- https://sqlzoo.net/wiki/SELECT_basics
+
 -- 1. Modify it to show the population of Germany
 SELECT population FROM world
 WHERE name = 'Germany';
 
--- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+-- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'. 
 SELECT name, population FROM world
 WHERE name IN('Sweden', 'Norway', 'Denmark');
 
