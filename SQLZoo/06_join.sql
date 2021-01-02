0d282e0d4806829b21643449d3590995b5e5bba8
Sara Pesavento <sarapesavento@gmail.com>
2021-01-02 13:50:14 -0800

    Update 06_join

--- SQLZoo/06_join.sql
+++ SQLZoo/06_join.sql
@@ -43,8 +43,9 @@ SELECT stadium, COUNT(player) FROM game JOIN goal ON(id=matchid)
 GROUP BY stadium;
 
 -- 11. For every match involving 'POL', show the matchid, date and the number of goals scored.
-SELECT matchid,mdate, team1, team2, teamid FROM game JOIN goal ON matchid = id
-WHERE (team1 = 'POL' OR team2 = 'POL';
+SELECT matchid, mdate, COUNT(teamid) as Goals FROM game JOIN goal ON(matchid = id)
+WHERE team1 = 'POL' OR team2 = 'POL'
+GROUP BY matchid, mdate;
 
 -- 12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'.
 SELECT matchid, mdate, COUNT(player) FROM goal JOIN game ON(matchid=id)
